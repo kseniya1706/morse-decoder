6f193308494cb917a4ad864c7330b9f67c91224d
Kseniya <55946556+kseniya1706@users.noreply.github.com>
2019-10-13 10:53:25 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -39,6 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decodedString = '';
+    
+    nextSymbol: for (let i = 0; i < expr.length; i += 10) {
+        let morseSymbol = '';
+        for (let j = i; j < i + 10; j += 2) {
+                if (expr[j] == '0') {
+                    morseSymbol += '';
+                }
+                else {
+                    if (expr[j+1] == '0') {
+                        morseSymbol += '.';
+                    }
+                    else {
+                        if (expr[j+1] == '1') {
+                            morseSymbol += '-';
+                        }
+                        else{
+                        decodedString += ' '; 
+                        continue nextSymbol;
+                        }
+                    }
+                }
+            }
+            decodedString += MORSE_TABLE[morseSymbol];
+        }
+    return decodedString;
 }
 
 module.exports = {
